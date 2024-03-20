3d67a022cf8703d09a1e2c85a3993a844d9aca18
jvarghe <vcs.inbox@outlook.com>
2024-03-20 15:40:08 -0400

    Add file '6_github_activity.sql' to directory '1_queries'.

---
+++ 1_queries/6_github_activity.sql
@@ -0,0 +1,9 @@
+-- Once a student graduates, the career services department needs to keep track
+-- of their activity on Github. If a graduate has not linked their Github
+-- account, we need to contact them to do so. Get all graduates without a linked
+-- Github account.
+
+SELECT name, email, phone
+FROM students
+WHERE github IS NULL
+AND end_date IS NOT NULL;
