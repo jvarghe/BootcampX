523979773c005363017a4b39c75f3471a1f83205
jvarghe <vcs.inbox@outlook.com>
2024-03-22 20:06:40 -0400

    Add comment to '5_average_assignment_completion_time.sql'.

--- 3_queries_group-by/5_average_assignment_completion_time.sql
+++ 3_queries_group-by/5_average_assignment_completion_time.sql
@@ -28,7 +28,7 @@ WHERE students.end_date IS NULL
 GROUP BY student
 ORDER BY average_assignment_duration DESC;
 
-
+-- Official Answer:
 -- SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
 -- FROM students
 -- JOIN assignment_submissions ON student_id = students.id
