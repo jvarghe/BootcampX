227a65d324a5d22701dbec41eae353b5f0c49118
jvarghe <vcs.inbox.outlook.com>
2024-04-25 18:11:05 -0400

    Update 'students.js' to implement parameterized queries.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,3 +1,13 @@
+/* PARAMETERIZED QUERIES
+ *
+ * This file has had its contents modified to implement parameterized queries.
+ * If you want to compare with a file that uses unsanitized input, see
+ * `teachers.js`.
+ *
+ * Always use parameterized queries when you have data that comes from an
+ * untrusted source, which is pretty much every source.
+ */
+
 const { Pool } = require("pg");
 
 
@@ -16,6 +26,10 @@ const cohort = process.argv[2];
 // falsy, such as undefined), then the value `5` will be used as the default
 // limit.
 const limit = process.argv[3] || 5;
+
+// Store the input values in an array so that database query can insert them
+// into a query as parameters (parameterized queries).
+const inputValues = [`%${cohort}%`, limit];
 
 
 // QUERIES
@@ -48,8 +62,12 @@ const query2 =
     cohorts.name AS cohort_name
   FROM students
     JOIN cohorts ON cohorts.id = students.cohort_id
-  WHERE cohorts.name LIKE '%${cohort}%'
-  LIMIT ${limit};
+  WHERE cohorts.name LIKE $1
+  LIMIT $2
+
+  -- This is what unsanitized input looks like:
+  -- WHERE cohorts.name LIKE '%${cohort}%'
+  -- LIMIT ${limit};
   `
   ;
 
@@ -60,7 +78,7 @@ const query2 =
 // Using the ` (backtick), we can write a multi line string like this to make
 // our SQL look nicer. The function then returns a promise that contains our
 // result when the query is successful.
-pool.query(query2)
+pool.query(query2, inputValues)
 
   // Records are being returned from the database, but with one important
   // note: the data is returned as an array of JavaScript objects. The `pg`
