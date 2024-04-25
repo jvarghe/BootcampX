b6adf1a250f5bb678dd1e0c99abb99401ff7d4ef
jvarghe <vcs.inbox.outlook.com>
2024-04-25 17:51:23 -0400

    Adjust style in 'students.js'.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -60,22 +60,22 @@ const query2 =
 // Using the ` (backtick), we can write a multi line string like this to make
 // our SQL look nicer. The function then returns a promise that contains our
 // result when the query is successful.
-pool.query(query2
+pool.query(query2)
 
   // Records are being returned from the database, but with one important
   // note: the data is returned as an array of JavaScript objects. The `pg`
   // package and others handle the conversion of data from the database to
   // JavaScript.
-).then((result) => {
+  .then((result) => {
 
-  // Print out records as JS objects in the terminal.
-  // console.log(result.rows);
+    // Print out records as JS objects in the terminal.
+    // console.log(result.rows);
 
 
-  // Query 1 & 2: Format and print out student and cohort information.
-  result.rows.forEach(student => {
-    console.log(`${student.name} has an ID of ${student.id} and is in the ${student.cohort_name} cohort.`);
-  });
+    // Query 1 & 2: Format and print out student and cohort information.
+    result.rows.forEach(student => {
+      console.log(`${student.name} has an ID of ${student.id} and is in the ${student.cohort_name} cohort.`);
+    });
 
-})
+  })
   .catch((errorObj) => console.error("Query had an error:", errorObj.stack));
