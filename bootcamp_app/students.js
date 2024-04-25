81e11dc939ed6c8da1f8857eba32738997b7958a
jvarghe <vcs.inbox.outlook.com>
2024-04-25 12:49:10 -0400

    Add first query to 'students.js'.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -17,21 +17,34 @@ const pool = new Pool({
 // our SQL look nicer. The function then returns a promise that contains our
 // result when the query is successful.
 pool.query(
+
+  // Query the `students` & `cohorts` tables:
   `
-  SELECT students.id,
-         students.name,
-         students.cohort_id,
-         cohorts.name AS cohort_name
+  SELECT
+        students.id,
+        students.name,
+        students.cohort_id,
+        cohorts.name AS cohort_name
   FROM students
     JOIN cohorts ON cohorts.id = students.cohort_id
-  LIMIT 5;
+  LIMIT 50;
   `
-)
-.then((result) => {
-  // Records are being returned from the database, but with one important
-  // note: the data is returned as an array of JavaScript objects. The `pg`
-  // package and others handle the conversion of data from the database to
-  // JavaScript.
-  console.log(result.rows);
-})
-.catch((errorObj) => console.error("Query had an error:", errorObj.stack));
+
+// Records are being returned from the database, but with one important
+// note: the data is returned as an array of JavaScript objects. The `pg`
+// package and others handle the conversion of data from the database to
+// JavaScript.
+).then((result) => {
+
+    // Print out records as JS objects in the terminal.
+    // console.log(result.rows);
+
+
+    // Format and print out student and cohort information.
+    result.rows.forEach(student => {
+      console.log(`${student.name} has an ID of ${student.id} and is in the ${student.cohort_name} cohort.`);
+    });
+
+
+  })
+  .catch((errorObj) => console.error("Query had an error:", errorObj.stack));
